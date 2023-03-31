759a5975cb24115ff30d05d696199bf2e891b1f1
tehwenyi <teh.wenyi22@gmail.com>
2023-03-31 11:22:36 +0800

    add bash script for train_aux.py for easy training

---
+++ train_aux.sh
@@ -0,0 +1,25 @@
+EXPR_PROJECT='proxy_aux'
+
+EXPR_BATCHSIZE=32
+EXPR_EPOCHS=300
+EXPR_IMGSIZE=640
+
+DATA='data/custom_sv.yaml'
+CFG='cfg/training/yolov7-custom.yaml'
+HYP='data/hyp.scratch.custom.yaml'
+WEIGHTS='weights/yolov7-e6e_training.pt'
+
+NUM_WORKERS=8
+DEVICE=0
+
+python train_aux.py \
+  --workers $NUM_WORKERS \
+  --device $DEVICE \
+  --batch-size $EXPR_BATCHSIZE \
+  --epochs $EXPR_EPOCHS \
+  --data $DATA \
+  --img-size $EXPR_IMGSIZE \
+  --cfg $CFG \
+  --weights $WEIGHTS \
+  --hyp $HYP \
+  --name $EXPR_PROJECT'_'$EXPR_IMGSIZE'_bs'$EXPR_BATCHSIZE'_e'$EXPR_EPOCHS
